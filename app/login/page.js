f9cfb6a73d35de5f329bcea690749e1ba97ff2ef
Ujjwal Kushwaha <111581344+zbytes2227@users.noreply.github.com>
2024-12-20 21:57:25 +0530

    Update login input type page.js

--- app/login/page.js
+++ app/login/page.js
@@ -93,7 +93,7 @@ const Login = () => {
 
                 <div>
                   <label htmlFor="contact" className="block mb-2 text-sm font-medium text-gray-900 drk:text-white">User Name</label>
-                  <input value={UserContact} onChange={(e) => setUserContact(e.target.value)} type="tel" name="contact" id="contact" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 drk:bg-gray-700 drk:border-gray-600 outline-none drk:placeholder-gray-400 drk:text-white drk:focus:ring-blue-500 drk:focus:border-blue-500" placeholder="enter username" required="" />
+                  <input value={UserContact} onChange={(e) => setUserContact(e.target.value)} type="text" name="contact" id="contact" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 drk:bg-gray-700 drk:border-gray-600 outline-none drk:placeholder-gray-400 drk:text-white drk:focus:ring-blue-500 drk:focus:border-blue-500" placeholder="enter username" required="" />
                 </div>
 
 
